ae9e281fe3c2f684a15d54aebe239af25ff5c905
Darsh Thakur <thakurdarsh21@gmail.com>
2024-10-11 02:35:22 +0530

    set up of the reducers and actions for the dynamic tweet functions like retweeting and crud with updating user associations

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,11 +2,14 @@ import { appBarClasses } from "@mui/material";
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
+import { tweetReducer } from "./Tweet/Reducer";
 
 
 const rootReducers=combineReducers({
 
     auth:authReducer, //this is like key given to access the state in the reducer using the auth key function of redux
+    tweet:tweetReducer,
+
 
     
 
